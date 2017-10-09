fbfd9a350d9c69cb9b7f97a7e86847ffd5dfe5e7
shiri castro <shiricastro3@gmail.com>
2017-10-09 16:35:19 +0300

    add tasks component

--- javascript.js
+++ javascript.js
@@ -1,18 +1,20 @@
-new Vue({
-	el:'#root',
-	data:{
-		newTask:'',
-		tasks:[
-			{description:'Go to the store',completed:true,editMode:false},
-			{description:'Clear inbox',completed:false,editMode:false},
-			{description:'Make dinner',completed:false,editMode:false}
-		]
+Vue.component('tasks',{
+	template:`<ul class="todoList">
+				<li class="active" v-for="(task,key) in tasks" :class="{'strikeLi':task.completed ,editing: task.editMode }">
+					<div class="view">
+                        <input type="checkbox" v-model="task.completed" />
+                        <span :class="{'strike':task.completed}" @click="turnOnEditMode(key)">{{task.description}}</span>
+                        <button @click="deleteTodo(key)">✖</button>
+                    </div>
+                    <input class="edit" type="text" v-model="task.description" name="edit" @keyup="$event.keyCode == 13 ? turnOffEditMode(key) : null" @blur="turnOffEditMode(key)">
+				</li>
+			</ul>`,
+
+	props: ['alltasks'],
+	data: function () {
+	  return { tasks: this.alltasks }
 	},
 	methods:{
-		addTask(){
-			this.tasks.push({description:this.newTask,completed:false,editMode:false});
-			this.newTask='';
-		},
 		turnOnEditMode(key){
             this.tasks[key].editMode = true;
         },
@@ -22,5 +24,30 @@ new Vue({
         deleteTodo(key){
         	this.tasks.splice(key,1);
         } 
+	}
+});
+
+
+function funTasks(){
+	var tasks=[
+			{description:'Go to the store',completed:true,editMode:false},
+			{description:'Clear inbox',completed:false,editMode:false},
+			{description:'Make dinner',completed:false,editMode:false}
+		]
+		return tasks
+}
+
+
+new Vue({
+	el:'#root',
+	data:{
+		newTask:'',
+		tasks:funTasks()
+	},
+	methods:{
+		addTask(){
+			this.tasks.push({description:this.newTask,completed:false,editMode:false});
+			this.newTask='';
+		}
 	}
 })
