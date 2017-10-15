b14a4e15b1fb61b4e7155f3b69af2ed60dd11bd4
shiri castro <shiricastro3@gmail.com>
2017-10-15 14:41:42 +0300

    add task component

--- javascript.js
+++ javascript.js
@@ -1,38 +1,44 @@
 Vue.component('tasks',{
 	template:`<ul class="todoList">
-				<li class="active" v-for="(task,key) in tasks" :class="{'strikeLi':task.completed ,editing: task.editMode }">
-					<div class="view">
-                        <input type="checkbox" v-model="task.completed" />
-                        <span :class="{'strike':task.completed}" @click="turnOnEditMode(key)">{{task.description}}</span>
-                        <button @click="deleteTodo(key)">✖</button>
-                    </div>
-                    <input class="edit" type="text" v-model="task.description" name="edit" @keyup="$event.keyCode == 13 ? turnOffEditMode(key) : null" @blur="turnOffEditMode(key)">
-				</li>
+				<li is="task" :task="task" :foo="tasks" @update:foo="deleteTodo()" v-for="task in tasks" :key="task.id" ></li>
 			</ul>`,
-
-	props: ['alltasks'],
-	data: function () {
-	  return { tasks: this.alltasks }
-	},
-	methods:{
-		turnOnEditMode(key){
-            this.tasks[key].editMode = true;
-        },
-        turnOffEditMode(key){
-            this.tasks[key].editMode = false;
-        },
+	props: ['tasks'],
+	methods: {
         deleteTodo(key){
         	this.tasks.splice(key,1);
         } 
-	}
+	},
 });
+Vue.component('task',{
+	template:`<li class="active" :class="{'strikeLi':task.completed ,editing: task.editMode }">
+					<div class="view">
+                        <input type="checkbox" v-model="task.completed" />
+                        <span :class="{'strike':task.completed}" @click="turnOnEditMode()">{{task.description}}</span>
+                        <button @click="deleteTodo()">✖</button>
+                    </div>
+                    <input class="edit" type="text" v-model="task.description" name="edit" @keyup="$event.keyCode == 13 ? turnOffEditMode() : null" @blur="turnOffEditMode()">
+				</li>`,
 
+	props: ['task'],
+	methods: {
+	    turnOnEditMode: function(){
+	    	this.task.editMode = true;
+	    },
+	    turnOffEditMode(){
+	        this.task.editMode = false;
+	    }, 
+        deleteTodo(key){
+        	this.$emit('update:foo', key);
+        } 
+	},
+
+});
 
 function funTasks(){
 	var tasks=[
-			{description:'Go to the store',completed:true,editMode:false},
-			{description:'Clear inbox',completed:false,editMode:false},
-			{description:'Make dinner',completed:false,editMode:false}
+			{id:0,description:'Go to the store',completed:true,editMode:false},
+			{id:1,description:'Clear inbox',completed:false,editMode:false},
+			{id:2,description:'Make dinner',completed:false,editMode:false}
 		]
 		return tasks
 }
